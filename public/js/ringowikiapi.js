3bff8266e660a52c1fae467cb61bfca0abba21f0
Yuki Kimoto <kimoto.yuki@gmail.com>
2012-04-10 15:59:11 +0900

    ページの編集ページを更新

--- public/js/ringowikiapi.js
+++ public/js/ringowikiapi.js
@@ -3,11 +3,17 @@ ringowikiapi.create_form_data = function (selector) {
   var form = $(selector);
   var params = {};
   form.find('input').each(function () {
-    var type = $(this).type || '';
-    var name = $(this).attr('name');
-    if (type === '' || type === 'password') {
+    var type = $(this).attr("type") || '';
+    if (type === '' || type === 'password' || type === 'hidden') {
+      var name = $(this).attr('name');
       params[name] = $(this).val();
     }
   });
+  
+  form.find('textarea').each(function () {
+    var name = $(this).attr('name');
+    params[name] = $(this).val();
+  });
+  
   return params;
 };
