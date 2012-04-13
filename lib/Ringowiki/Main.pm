0fc2e2f8cce0cd8cfda3b309c580e0cf9f7b6e53
Yuki Kimoto <kimoto.yuki@gmail.com>
2012-04-13 19:12:03 +0900

    ページの内容を表示するようにした

--- lib/Ringowiki/Main.pm
+++ lib/Ringowiki/Main.pm
@@ -31,11 +31,43 @@ sub index {
 
 sub page {
   my $self = shift;
+
+  # Validation
+  my $raw_params = {map { $_ => $self->param($_) } $self->param};
+  my $rule = [
+    wiki_id => ['word'],
+    page_name => ['not_blank']
+  ];
+  my $vresult = $self->app->validator->validate($raw_params, $rule);
+  my $params = $vresult->data;
+
+  # DBI
+  my $dbi = $self->app->dbi;
   
-  my $wiki_id = 'main';
-  my $page_name = '';
+  # Wiki id
+  my $wiki_id = $params->{wiki_id};
+  unless (defined $wiki_id) {
+    $wiki_id = $dbi->model('wiki')->select('id', append => 'order by main desc')->value;
+  }
   
-  $self->render(wiki_id => $wiki_id, page_name => $page_name);
+  # Page name
+  my $page_name = $params->{page_name};
+  unless (defined $page_name) {
+    $page_name = $dbi->model('page')->select(
+      'name',
+      where => {wiki_id => $wiki_id},
+      append => 'order by main desc'
+    )->value;
+  }
+  
+  # Page
+  my $page = $dbi->model('page')->select(
+    where => {wiki_id => $wiki_id, name => $page_name},
+  )->one;
+
+  return $self->render_not_found unless defined $page;
+  
+  $self->render(page => $page);
 }
 
 1;
